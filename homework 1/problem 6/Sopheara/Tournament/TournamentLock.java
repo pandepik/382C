f0367f9b44395f0af623e5346e29385242c717d3
Sopheara <sduong1125@gmail.com>
2018-09-12 21:47:42 -0500

    update

--- homework 1/problem 6/Sopheara/Tournament/TournamentLock.java
+++ homework 1/problem 6/Sopheara/Tournament/TournamentLock.java
@@ -5,35 +5,40 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TournamentLock implements Lock {
     
 	static int threads;
+	AtomicInteger[] level;
 	AtomicInteger[] last;
-	AtomicInteger[] levels;
 	
+
 	public TournamentLock(int numThreads){
         threads = numThreads;
-        last = new AtomicInteger[numThreads];
-		levels = new AtomicInteger[numThreads];
+        last = new AtomicInteger[threads];
+		level = new AtomicInteger[threads];
 		for(int i = 0; i < threads; i++) {
 			last[i] = new AtomicInteger();
-			levels[i] = new AtomicInteger();
+			level[i] = new AtomicInteger();
 		}
-    }
-       
-    public void unlock(int pid) {
-    	levels[pid].set(0);
+		for(int i = 0; i < threads; i++) {
+			level[i].set(0);
+			last[i].set(0);
+		}
     }
     
     public void lock(int pid) {
     	for(int i = 0; i < threads; i++) {
-    		levels[pid].set(i);
+    		level[pid].set(i);
     		last[i].set(pid);
     		for(int k = 0; k < threads; k++) {
-    			while(last[i].get() == pid && (k != pid && levels[k].get() >= i)) {
+    			while((last[i].get() == pid) && (k != pid) && (level[k].get() >= i)) {
     				if(!PIncrement.check()) {
     					break;
     				}
     			}
     		}
     	}
+    }
+    
+    public void unlock(int pid) {
+    	level[pid].set(0);
     }
     
 }
